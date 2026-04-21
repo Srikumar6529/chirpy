aa3a6852391d0b7fdf2e977e0ea6831aab1c6ad3
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-04-21 12:12:01 -0500

    added index.html file to the server and serving it on root path

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	const port = "8080"
+	const filePathRoot = "."
 	mux := http.NewServeMux()
+	mux.Handle("/",http.FileServer(http.Dir(filePathRoot)))
 	srv := &http.Server{
-		Addr: ":" + port,
-		Handler : mux,
+		Addr:    ":" + port,
+		Handler: mux,
 	}
-	log.Printf("Serving on port: %s\n",port)
+	log.Printf("Serving files from %s port: %s\n",filePathRoot,port)
 	log.Fatal(srv.ListenAndServe())
 }
